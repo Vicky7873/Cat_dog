7570786cb92fa1e73eefa8cbff8fc9e3ab718626
Bhiki Pallai <bhikipallai@Bhikis-MacBook-Air.local>
2024-09-02 21:54:04 +0530

    env and project str added

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Cat_dog
+
+### Create an env
+1. conda create -n catdog python=3.12 -y 
+2. Create an temp.py where our folder dtr will added 
+
---
+++ temp.py
@@ -0,0 +1,32 @@
+import os
+import sys
+from pathlib import Path
+
+
+list_of_folders = [
+    ".github/workflows/.gitkeep",
+    "data/.gitkeep",
+    f"src/__init__.py",
+    f"src/utils/__init__.py",
+    f"src/utils/common.py",
+    f"src/components/__init__.py",
+    f"src/logging/__init__.py",
+    f"src/config/__init__.py",
+    f"src/config/configuration.py",
+    f"src/pipeline/__init__.py",
+    f"src/entity/__init__.py",
+    f"src/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "setup.py",
+    "research/trials.ipynb",
+    "templates/index.html"
+]
+
+for folder in list_of_folders:
+    path = Path(folder)
+    path.parent.mkdir(parents=True, exist_ok=True)
+    if not path.exists():
+        with open(path, "w") as f:
+            pass
+        print(f"Created: {path}")
